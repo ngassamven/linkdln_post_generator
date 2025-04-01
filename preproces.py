bc785e6789496fa2b86b8b631166b9ff3781582c
ngassamven <venceslas.kateur@gmail.com>
2025-04-01 10:46:27 +0200

    ajoit des fichiers

--- preproces.py
+++ preproces.py
@@ -8,7 +8,7 @@ from langchain_core.exceptions import OutputParserException
 def process_posts(raw_file_path, processed_file_path=None):
     with open(raw_file_path, encoding='utf-8') as file:
         posts = json.load(file)
-        
+        print(posts)
 
 if __name__ == "__main__":
     process_posts("data/raw_posts.json", "data/processed_posts.json")
