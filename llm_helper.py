3f9381f9a1627fb42c7108f861fab7d2a296762d
ngassamven <venceslas.kateur@gmail.com>
2025-04-01 13:04:18 +0200

    ajout des fichiers

--- llm_helper.py
+++ llm_helper.py
@@ -1,13 +1,12 @@
+from langchain_groq import ChatGroq
+import os
 from dotenv import load_dotenv
-from langchain_grop import ChatGroq
-import os 
-
 
 load_dotenv()
-
-llm = ChatGroq(groq_api_key=os.getenv("GROQ_API_KEY"), model_name="llama-3.3-70b-versatile")
-
+llm = ChatGroq(groq_api_key=os.getenv("GROQ_API_KEY"), model_name="mistral-saba-24b")
 
 if __name__ == "__main__":
-    response = llm.invoke("Two most important ingradient in samosa are ")
+    query = "Two most important ingredients in samosa are"  # Assurez-vous que query est une chaîne de caractères
+    # Pas besoin de `encode('utf-8')` ici, car `llm.invoke()` prend déjà des chaînes de caractères en entrée.
+    response = llm.invoke(query)  # Envoie directement la chaîne
     print(response.content)
